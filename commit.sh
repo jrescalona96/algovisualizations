8b98cbd2f43706d7b8e42429c38e9761bf2d11ab
John Richmond Escalona <mond@Johns-MacBook-Pro.local>
2020-07-12 11:46:20 -0700

    refactor commit.sh

--- commit.sh
+++ commit.sh
@@ -1,8 +1,12 @@
 #commit, merge, push changes from dev
+printf "\n====================================================== stage changes =========================================================\n"
 git add .
-git commit -m "$1" && echo "commit with $1"
+printf "\n====================================================== commit changes ========================================================\n"
+git commit -m "$1" && printf "commit with $1"
+printf "\n=================================================== merged master to dev =====================================================\n"
 git checkout master && git merge dev
-echo "merged changes"
+printf "\n====================================================== merged changes ========================================================\n"
 git push
-echo "pushed changes"
+printf "\n====================================================== pushed changes ========================================================\n"
 git checkout dev
+printf "All Done!!"
