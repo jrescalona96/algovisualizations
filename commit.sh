a39a1f12102f1ec54c4729178fbff415450a2cbd
John Richmond Escalona <mond@Johns-MacBook-Pro.local>
2020-07-12 11:41:32 -0700

    added commit script

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #commit, merge, push changes from dev
 git add .
-git commit -m "$1" && echo "$1"
+git commit -m "$1" && echo "commit with $1"
 git checkout master && git merge dev
 echo "merged changes"
 git push
