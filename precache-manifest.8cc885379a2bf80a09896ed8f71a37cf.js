cf3517fb1c8bc9436fa47bb8ee3d78973a98fa39
John Richmond Escalona <mond@Johns-MacBook-Pro.local>
2020-07-28 22:17:16 -0700

    Updates

--- precache-manifest.8cc885379a2bf80a09896ed8f71a37cf.js
+++ precache-manifest.8cc885379a2bf80a09896ed8f71a37cf.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "514c0321fa45ffb760e55fd860df0364",
+    "revision": "7ad9ad603732266b3bb74e65115efa7d",
     "url": "/algovisualizations/index.html"
   },
   {
-    "revision": "cc2c53d1a464c0c89168",
+    "revision": "5bab69b7840b41d40de3",
     "url": "/algovisualizations/static/css/main.63e9b80a.chunk.css"
   },
   {
-    "revision": "324883df9baa10e62dfe",
-    "url": "/algovisualizations/static/js/2.be551c00.chunk.js"
+    "revision": "46c2396a43c13b2323be",
+    "url": "/algovisualizations/static/js/2.4673094f.chunk.js"
   },
   {
     "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
-    "url": "/algovisualizations/static/js/2.be551c00.chunk.js.LICENSE.txt"
+    "url": "/algovisualizations/static/js/2.4673094f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cc2c53d1a464c0c89168",
-    "url": "/algovisualizations/static/js/main.dc04fcd0.chunk.js"
+    "revision": "5bab69b7840b41d40de3",
+    "url": "/algovisualizations/static/js/main.d8fdfc02.chunk.js"
   },
   {
     "revision": "febb49d0c978252d744c",
