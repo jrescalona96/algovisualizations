e00a672aab2792066b3fd2af1e7640b3ec44ac6b
John Richmond Escalona <mond@Johns-MacBook-Pro.local>
2020-08-09 20:01:28 -0700

    edit gitignore

--- commit.sh
+++ commit.sh
@@ -1,12 +1,12 @@
-#commit, merge, push changes from dev
+
+printf "\n=================== merged master to dev =========================\n\n"
+git checkout master && git merge dev
 git add .
 printf "\n====================== stage changes =============================\n\n"
 git status
 printf "\n====================== committing changes ========================\n\n"
 printf "commit with $1"
 git commit -m "$1"
-printf "\n=================== merged master to dev =========================\n\n"
-git checkout master && git merge dev
 printf "\n====================== merged changes ============================\n\n"
 git push
 printf "\n====================== pushed changes ============================\n\n"
